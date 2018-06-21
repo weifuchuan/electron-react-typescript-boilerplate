bb6a92926eab87b404ef123b06b56d6239a3448f
weifuchuan <fuchuan.wei@hotmail.com>
2018-06-21 15:49:52 +0800

    add initer

--- init.js
+++ init.js
@@ -1,5 +1,28 @@
 "use strict";
 exports.__esModule = true;
 var fs = require("fs");
-fs.rmdirSync("app");
+var path = require("path");
+function rmDir(filepath) {
+    // Uncatch exception, if someone unaccessable, it should throw
+    fs.accessSync(filepath);
+    var stat = fs.statSync(filepath);
+    if (stat.isDirectory) {
+        fs.readdirSync(filepath).forEach(function (file) {
+            var filepath2 = path.join(filepath, file);
+            fs.accessSync(filepath);
+            var stat = fs.statSync(filepath);
+            if (stat.isDirectory()) {
+                rmDir(filepath2);
+            }
+            else {
+                fs.unlinkSync(filepath2);
+            }
+        });
+        fs.rmdirSync(filepath);
+    }
+    else {
+        return;
+    }
+}
+rmDir("app");
 fs.renameSync("__init_app", "app");
---
+++ init.ts
@@ -0,0 +1,26 @@
+import fs = require("fs");
+import path = require("path");
+
+function rmDir(filepath: string) {
+  // Uncatch exception, if someone unaccessable, it should throw
+  fs.accessSync(filepath);
+  const stat = fs.statSync(filepath);
+  if (stat.isDirectory) {
+    fs.readdirSync(filepath).forEach(file => {
+      const filepath2 = path.join(filepath, file);
+      fs.accessSync(filepath);
+      const stat = fs.statSync(filepath);
+      if (stat.isDirectory()) {
+        rmDir(filepath2);
+      } else {
+        fs.unlinkSync(filepath2);
+      }
+    });
+    fs.rmdirSync(filepath);
+  } else {
+    return;
+  }
+}
+
+rmDir("app");
+fs.renameSync("__init_app", "app");
